22cea2563e013063053e7a5c2c6b7a70c9936c5d
Max <maxanderberg@gmail.com>
2021-06-17 08:01:36 +0200

    cleaned up the navbar code

--- src/layout/navbar.js
+++ src/layout/navbar.js
@@ -1,14 +1,5 @@
 import logo from '../images/Mob_The_Builders.png';
 
-/*
-Need to import an image here
-----
-1. Create a navbar with JS
-    a. Use the image in the navbar
-2. Export the navbar 
-3. Import it / inject it to index.js 
-*/
-
 const navbar = document.createElement('header');
 navbar.className = 'navbar'
 
@@ -17,7 +8,5 @@ img.src = logo;
 img.className = 'navbar__logo';
 
 navbar.appendChild(img);
-
-
 
 export { navbar };
