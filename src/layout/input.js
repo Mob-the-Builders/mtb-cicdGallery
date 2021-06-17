2199cd82792544325955fb6123041cd5e284784f
Max <maxanderberg@gmail.com>
2021-06-17 08:28:46 +0200

    Polished the searchbar

--- src/layout/input.js
+++ src/layout/input.js
@@ -2,6 +2,7 @@
 const input = () => {
   const section = document.createElement('section');
   section.id = 'searchBar';
+  section.className = "search-bar__container-flex"
   section.innerHTML = '<input type="search" results=5 autosave=some_unique_value name="s" id="value" value="london"><input type="submit" id="submit">'
   return section;
 }
